b55742831965a190535d0e41567717132f1fea5d
Zo7Chen <qq745481293@gmail.com>
2017-05-31 12:05:37 +0800

    ....

--- src/courseselectionUI/UIController.java
+++ src/courseselectionUI/UIController.java
@@ -13,7 +13,7 @@ import courseselectionsystem.*;
  */
 public class UIController  implements CourseSelectionSystem.EntrySelectionHandler {
     UserTypeSelectPage usertypeselectpage;
-
+    CourseSelectionSystem.Entry entry = CourseSelectionSystem.Entry.unknown;
    
     public UIController(){
 
@@ -24,7 +24,35 @@ public class UIController  implements CourseSelectionSystem.EntrySelectionHandle
     @Override
     public CourseSelectionSystem.Entry handle(){
         usertypeselectpage.setVisible(true);
-        return usertypeselectpage.get_entry();
+        
+        
+        
+        for (; ; ){
+            new Thread(new Runnable(){
+            @Override
+            public void run(){
+                //for (;;){
+                    System.out.println("查看有没有按下");
+                    entry = usertypeselectpage.get_entry();
+                    try{
+                       Thread.sleep(10);
+                    }catch(Exception e){
+                        
+                    }
+               // }
+            }
+            
+        }).start();
+            System.out.println("按下了没");
+            if(entry !=  CourseSelectionSystem.Entry.unknown){
+                
+                System.out.println("返回了");
+                usertypeselectpage.setVisible(false);
+                //usertypeselectpage.setVisible(false);
+                return entry;
+            }
+        }
+        
     }
     
 }
